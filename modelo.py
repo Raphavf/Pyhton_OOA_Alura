b003409b5f4c52349aa117af14ca37c05993d360
Raphavf <47055553+Raphavf@users.noreply.github.com>
2022-08-24 21:54:34 -0300

    Adicionando properties e setter

--- modelo.py
+++ modelo.py
@@ -1,11 +1,43 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+    def dar_likes(self):
+        self.__likes += 1
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
 
 class Series:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    def dar_likes(self):
+        self.__likes += 1
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
