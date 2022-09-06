5c718e986cb53e0fc1f786861af6caa64a9f420e
Raphavf <47055553+Raphavf@users.noreply.github.com>
2022-09-05 21:35:51 -0300

    herança e superclasse

--- modelo.py
+++ modelo.py
@@ -4,20 +4,20 @@ class Programa:
         self.ano = ano
         self._likes = 0
 
-    def dar_likes(self):
-        self._likes += 1
-
     @property
     def likes(self):
         return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
 
     @property
     def nome(self):
         return self._nome
 
     @nome.setter
-    def nome(self, novo_nome):
-        self._nome = novo_nome
+    def nome(self, nome):
+        self._nome = nome
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
@@ -26,7 +26,17 @@ class Filme(Programa):
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        super().__init__(nome,ano)
+        super().__init__(nome, ano)
         self.temporadas = temporadas
 
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
