aede501e79a10b06636dbd4dd2a52a94df7566cf
Raphavf <47055553+Raphavf@users.noreply.github.com>
2022-09-13 22:04:39 -0300

    Criando o metodo imorimir para a classe Programa e suas herdeiras

--- modelo.py
+++ modelo.py
@@ -19,15 +19,24 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} D - {self._likes} Likes')
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes')
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes')
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -38,5 +47,10 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+#print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+#print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
