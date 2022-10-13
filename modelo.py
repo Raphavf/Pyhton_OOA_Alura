cb47bc95da58e25be697cd9dab8a367054f3498e
Raphavf <47055553+Raphavf@users.noreply.github.com>
2022-10-13 20:07:32 -0300

    dunder method len

--- modelo.py
+++ modelo.py
@@ -22,6 +22,8 @@ class Programa:
     def __str__(self):
         return f'{self._nome} - {self.ano} D - {self._likes} Likes'
 
+
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
@@ -38,13 +40,21 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes'
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
 
-    def tamanho(self):
-        return len(self.programas)
+    @property
+    def listagem(self):
+        return self._programas
+
+
+    def __len__(self):
+        return len(self._programas)
+
+    def __getitem__(self, item):
+        return self._programas[item]
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -73,7 +83,8 @@ playlist_fim_de_semana = Playlist('fds',filmes_e_series)
 
 print(f"Tamanho da playlist: {len(playlist_fim_de_semana)}")
 
+print(playlist_fim_de_semana[0])
+
 for programa in playlist_fim_de_semana:
     print(programa)
 
-print(f'Demolidor está na lista? {demolidor in playlist_fim_de_semana}')
